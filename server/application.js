2f91763a1be794f8c8c219f29247238470eeec8e
Oleg Shalygin <oshalygin@gmail.com>
2016-10-13 15:21:54 -0700

    building out the storm path auth endpoints

--- server/application.js
+++ server/application.js
@@ -1,54 +1,60 @@
 /* eslint-disable no-console */
-// import path from "path";
 import express from "express";
-// import bodyParser from "body-parser";
-// import webpack from "webpack";
-// import configuration from "../webpack.config.dev";
+
 import colors from "colors"; //eslint-disable-line no-unused-vars
 import open from "open";
-import { STORMPATH_API_KEY } from "../utilities/configurationKeys";
+import {
+    STORMPATH_APPLICATION_HREF,
+    STORMPATH_CLIENT_APIKEY_ID,
+    STORMPATH_CLIENT_APIKEY_SECRET
+} from "../utilities/configurationKeys";
 
-// import router from "./routes";
 
 let application = express();
 let stormpath = require("express-stormpath");
-
-application.use(stormpath.init(application, {
-    application: {
-        href: STORMPATH_API_KEY
-    }
-}));
-
 let port = process.env.PORT || 3000; //eslint-disable-line no-process-env
 
-// application.use(bodyParser.urlencoded({ extended: true }));
-// application.use(bodyParser.json());
+application.use(stormpath.init(application, {
+    apiKey: {
+        id: STORMPATH_CLIENT_APIKEY_ID,
+        secret: STORMPATH_CLIENT_APIKEY_SECRET
+    },
+    application: {
+        href: STORMPATH_APPLICATION_HREF
+    },
+    website: true
+}));
 
-
-
-// const applicationCompiler = webpack(configuration);
-// application.use(require("webpack-dev-middleware")(applicationCompiler, {
-//     noInfo: true,
-//     publicPath: configuration.output.publicPath
-// }));
-
-// application.use(require("webpack-hot-middleware")(applicationCompiler));
-
-// application.use("/api", router);
-// application.use("/client", express.static(path.join(__dirname, "../client")));
-
-// application.get("*", (request, response) => {
-//     let clientEntryPoint = path.join(__dirname, "../client/index.html");
-//     response.sendFile(clientEntryPoint);
-// });
-
-
-application.listen(port, (error) => {
-    if (!!error) {
-        console.log(error.bold.red);
-    }
-    open(`http://localhost:${port}`);
-    console.log(`Serving API AT http://localhost:${port}`.blue);
+application.get("/", stormpath.loginRequired, (request, response) => {
+    response.send(`Hey there, ${request.user.givenName}`);
 });
+
+application.get("/api", stormpath.loginRequired, (request, response) => {
+    request.user.createApiKey((error, apiKey) => {
+        if (!!error) {
+            return error;
+        }
+        return response.json({apiId: apiKey.id, apiSecret: apiKey.secret});
+    });
+
+});
+
+application.get("/api/test", stormpath.apiAuthenticationRequired, (request, response) => {
+    response.json({
+        data: "Hello World Data"
+    });
+});
+
+
+application.on("stormpath.ready", () => {
+    application.listen(port, (error) => {
+        if (!!error) {
+            console.log(error.bold.red);
+        }
+        open(`http://localhost:${port}`);
+        console.log(`Serving API AT http://localhost:${port}`.blue);
+    });
+});
+
 
 export default application;
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,12 +2,6 @@
 import express from "express"; //eslint-disable-line no-unused-vars
 
 export default function userController() {
-    return {
-        get
-    };
 
-    function get(request, response) {
-        let query = request.query;
 
-    }
 }
